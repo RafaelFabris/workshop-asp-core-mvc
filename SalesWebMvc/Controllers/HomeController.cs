7ce80a2aa6a2de29f9185b1c7e3175b5fbc0046e
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-21 23:17:50 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web App from C# Course";
+            ViewData["Aluno"] = "Rafael Fabris";
 
             return View();
         }
