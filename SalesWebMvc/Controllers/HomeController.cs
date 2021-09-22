980dbd7e4f406e4658efa56162bf097cb011ec9d
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-22 17:56:55 -0300

    Returning custom error page

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿ using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models.VielModels;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,10 +1,11 @@
 using System;
 
-namespace SalesWebMvc.Models.VielModels
+namespace SalesWebMvc.Models.ViewModels
 {
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
